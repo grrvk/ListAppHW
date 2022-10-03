456ed01fa095fae25bb2275f8daaadaea619a88e
Grvk <vika@granadzer.com>
2022-10-03 11:40:28 +0300

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Vika Granadzer on 03.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
